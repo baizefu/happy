5a36089f1298bb795d8ad46c90a52f29fe2f1a94
baizefu <gdc32675081@163.com>
2019-05-06 11:24:25 +0800

    在master分支下增加了一条输出语句

---
+++ admin.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		System.out.println("修改了HelloWorld类");
+		System.out.println("第二次修改");
+		System.out.println("在master分支下输出一条信息");
+	}
+}
